ebdb94a02995fc1168731a884e3fd0c1d6caf2f8
albert0 <fungalberto@gmail.com>
2017-09-30 13:25:51 -0500

    Commit directory

--- Assignment1/dcn_mnist.py
+++ Assignment1/dcn_mnist.py
@@ -23,8 +23,9 @@ def weight_variable(shape):
     '''
 
     # IMPLEMENT YOUR WEIGHT_VARIABLE HERE
+    initial = tf.truncated_normal(shape, stddev=0.1)
 
-    return W
+    return tf.Variable(initial)
 
 def bias_variable(shape):
     '''
@@ -35,8 +36,9 @@ def bias_variable(shape):
     '''
 
     # IMPLEMENT YOUR BIAS_VARIABLE HERE
+    initial = tf.constant(0.1, shape=shape)
 
-    return b
+    return tf.Variable(initial)
 
 def conv2d(x, W):
     '''
@@ -56,7 +58,7 @@ def conv2d(x, W):
 
     # IMPLEMENT YOUR CONV2D HERE
 
-    return h_conv
+    return tf.nn.conv2d(x,W, strides=[1, 1, 1, 1], padding='SAME')
 
 def max_pool_2x2(x):
     '''
@@ -67,7 +69,7 @@ def max_pool_2x2(x):
 
     # IMPLEMENT YOUR MAX_POOL_2X2 HERE
 
-    return h_max
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2,2, 1], padding='SAME')
 
 def main():
     # Specify training parameters
@@ -79,51 +81,51 @@ def main():
     # FILL IN THE CODE BELOW TO BUILD YOUR NETWORK
 
     # placeholders for input data and input labeles
-    x =
-    y_ =
+    x = tf.placeholder(tf.float32, shape=[None, 784])
+    y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
     # reshape the input image
-    x_image = tf.reshape(x, [-1, 28, 28, 1]
+    x_image = tf.reshape(x, [-1, 28, 28, 1])
 
     # first convolutional layer
-    W_conv1 =
-    b_conv1 =
-    h_conv1 =
-    h_pool1 =
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
 
     # second convolutional layer
-    W_conv2 =
-    b_conv2 =
-    h_conv2 =
-    h_pool2 =
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
 
     # densely connected layer
-    W_fc1 =
-    b_fc1 =
-    h_pool2_flat =
-    h_fc1 =
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     # dropout
-    keep_prob =
-    h_fc1_drop =
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
     # softmax
-    W_fc2 =
-    b_fc2 =
-    y_conv =
+    W_fc2 = weight_variable([1024, 10])
+    b_fc2 = bias_variable([10])
+    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
     # FILL IN THE FOLLOWING CODE TO SET UP THE TRAINING
 
     # setup training
-    cross_entropy =
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    correct_prediction =
-    accuracy =
+    correct_prediction = tf.equal(tf.arg_max(y_conv, 1), tf.arg_max(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
     # Add a scalar summary for the snapshot loss.
-    tf.scalar_summary(cross_entropy.op.name, cross_entropy)
+    tf.summary.scalar(cross_entropy.op.name, cross_entropy)
     # Build the summary operation based on the TF collection of Summaries.
-    summary_op = tf.merge_all_summaries()
+    summary_op = tf.summary.merge_all()
 
     # Add the variable initializer Op.
     init = tf.initialize_all_variables()
@@ -132,7 +134,7 @@ def main():
     saver = tf.train.Saver()
 
     # Instantiate a SummaryWriter to output summaries and the Graph.
-    summary_writer = tf.train.SummaryWriter(result_dir, sess.graph)
+    summary_writer = tf.summary.FileWriter(result_dir, sess.graph)
 
     # Run the Op to initialize the variables.
     sess.run(init)
--- Assignment1/n_layer_neural_network.py
+++ Assignment1/n_layer_neural_network.py
@@ -1,4 +1,39 @@
+#__author__ = Alberto Fung
 import numpy as np
 from sklearn import datasets, linear_model
 import matplotlib.pyplot as plt
 from three_layer_neural_network import NeuralNetwork
+
+def generate_data():
+    '''
+    generate data
+    :return: X: input data, y: given labels
+    '''
+    np.random.seed(0)
+    X, y = datasets.make_moons(200, noise=0.20)
+    return X, y
+
+def plot_decision_boundary(pred_func, X, y):
+    '''
+    plot the decision boundary
+    :param pred_func: function used to predict the label
+    :param X: input data
+    :param y: given labels
+    :return:
+    '''
+    # Set min and max values and give it some padding
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    h = 0.01
+    # Generate a grid min, y_max, h))
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole gid
+    Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
+    plt.show()
+
+class DeepNeuralNetwork(NeuralNetwork):
+
